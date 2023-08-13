4b36d36a1e15d29ac955363dc8a6ba8a4ceec0ea
Gabriel Martinez <gabjr2001@hotmail.com>
2023-08-13 16:40:07 -0600

    Creation of Trailer Component with react-player

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,6 +1,9 @@
 import './Hero.css';
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Hero = ({movies}) => {
     return (
@@ -18,6 +21,13 @@ const Hero = ({movies}) => {
                                             </div>
                                             <div className='movie-title'>
                                                 <h4>{movie.title}</h4>
+                                            </div>
+                                            <div className='movie-buttons-container'>
+                                                <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                                                    <div className='play-button-icon-container'>
+                                                        <FontAwesomeIcon className="play-button-icon" icon={faCirclePlay}/>
+                                                    </div>
+                                                </Link>
                                             </div>
                                         </div>
                                     </div>
